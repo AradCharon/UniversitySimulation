9639a9e4b923bc1d719b58d0c1c8f5c6e44c6f5c
aradcharon <aradshafiee06@gmail.com>
2025-06-18 01:28:45 +0330

    edit and adding a constructor for the PresentedCourse class

--- src/uni/PresentedCourse.java
+++ src/uni/PresentedCourse.java
@@ -3,6 +3,7 @@ package uni;
 import java.util.ArrayList;
 
 public class PresentedCourse {
+    private static int nextId = 1;
     public static ArrayList<PresentedCourse> presentedCourseList = new ArrayList<>();
 
     private final int id;
@@ -12,7 +13,12 @@ public class PresentedCourse {
     public ArrayList<Integer> students;
 
     public PresentedCourse(int courseID, int professorID, int capacity) {
-
+        this.id = nextId++;
+        this.courseID = courseID;
+        this.professorID = professorID;
+        this.capacity = capacity;
+        this.students = new ArrayList<>(capacity);
+        presentedCourseList.add(this);
     }
 
     public static PresentedCourse findById(int id) {
